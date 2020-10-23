aad19197a01e622c60291c70e167418a49991264
repl.it user <replituser@example.com>
2020-10-23 09:56:15 +0000

    some improvements

--- Zahlenfilter.java
+++ Zahlenfilter.java
@@ -1,24 +1,23 @@
 class Zahlenfilter{
 
   public static void main(String[] args) {
-    zahlenfeld();
+    zahlenfilter();
   }
 
-  public static void zahlenfeld(){
+  public static void zahlenfilter(){
     for(int i = 0; i < 200; i++)
     {
-      System.out.println("Zahl ="+i);
       if(i%5 == 0 && i != 0)
       {
-        System.out.println("Zahl ist durch 5 Teilbar!\n");
+        System.out.println(i +" ist durch 5 Teilbar!\n");
       }
       if(i%10 == 9)
       {
-        System.out.println("Zahl endet auf 9!\n");
+        System.out.println(i +" endet auf 9!\n");
       }
       if((i+(i-1)) %3 == 0)
       {
-        System.out.println("ZAHL und VORGÄNGER addiert ergeben ERGEBNIS und ERGEBNIS ist durch 3 teilbar\n");
+        System.out.println(i + " und " + (i-1) + " addiert ergeben " +(i+(i-1)) +" und "+(i+(i-1))+" ist durch 3 teilbar\n");
       }
     }
   }
